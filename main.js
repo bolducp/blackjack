c91f183338eb2518d3c38388986e2aed16f96f40
Paige Bolduc <paigebolduc@gmail.com>
2016-01-15 00:05:46 -0800

    working version, all mvp features satisfied

--- main.js
+++ main.js
@@ -6,46 +6,42 @@ function init(){
   $("#deal").click(deal);
   $("#hit").click(hit);
   $("#stay").click(stay);
-  //$("#nextHand").click()
-
+  $("#nextHand").click(nextHand);
 }
 
-
 var cardDeck = [{value: 11, image: "ace_of_spades.jpg"},{value: 2, image: "2_of_spades.jpg"},
-    {value: 3, image: "3_of_spades.jgp"}, {value: 4, image: "4_of_spades.jpg"},
+    {value: 3, image: "3_of_spades.jpg"}, {value: 4, image: "4_of_spades.jpg"},
     {value: 5, image: "5_of_spades.jpg"}, {value: 6, image: "6_of_spades.jpg"},
     {value: 7, image: "7_of_spades.jpg"}, {value: 8, image: "8_of_spades.jpg"},
     {value: 9, image: "9_of_spades.jpg"},{value: 10, image: "10_of_spades.jpg"},
     {value: 10, image: "jack_of_spades.jpg"},{value: 10, image: "queen_of_spades.jpg"},
     {value: 10, image: "king_of_spades.jpg"},{value: 11, image: "ace_of_clubs.jpg"},
-    {value: 2, image: "2_of_clubs.jpg"},{value: 3, image: "3_of_clubs.jgp"},
-    {value: 4, image: "4_of_clubs.jpg"},
-    {value: 5, image: "5_of_clubs.jpg"}, {value: 6, image: "6_of_clubs.jpg"},
-    {value: 7, image: "7_of_clubs.jpg"}, {value: 8, image: "8_of_clubs.jpg"},
-    {value: 9, image: "9_of_clubs.jpg"},{value: 10, image: "10_of_clubs.jpg"},
-    {value: 10, image: "jack_of_clubs.jpg"},{value: 10, image: "queen_of_clubs.jpg"},
-    {value: 10, image: "king_of_clubs.jpg"},
+    {value: 2, image: "2_of_clubs.jpg"},{value: 3, image: "3_of_clubs.jpg"},
+    {value: 4, image: "4_of_clubs.jpg"}, {value: 5, image: "5_of_clubs.jpg"},
+    {value: 6, image: "6_of_clubs.jpg"},{value: 7, image: "7_of_clubs.jpg"},
+    {value: 8, image: "8_of_clubs.jpg"}, {value: 9, image: "9_of_clubs.jpg"},
+    {value: 10, image: "10_of_clubs.jpg"}, {value: 10, image: "jack_of_clubs.jpg"},
+    {value: 10, image: "queen_of_clubs.jpg"},{value: 10, image: "king_of_clubs.jpg"},
     {value: 11, image: "ace_of_hearts.jpg"},{value: 2, image: "2_of_hearts.jpg"},
-    {value: 3, image: "3_of_hearts.jgp"}, {value: 4, image: "4_of_hearts.jpg"},
+    {value: 3, image: "3_of_hearts.jpg"}, {value: 4, image: "4_of_hearts.jpg"},
     {value: 5, image: "5_of_hearts.jpg"}, {value: 6, image: "6_of_hearts.jpg"},
     {value: 7, image: "7_of_hearts.jpg"}, {value: 8, image: "8_of_hearts.jpg"},
     {value: 9, image: "9_of_hearts.jpg"},{value: 10, image: "10_of_hearts.jpg"},
     {value: 10, image: "jack_of_hearts.jpg"},{value: 10, image: "queen_of_hearts.jpg"},
-    {value: 10, image: "king_of_hearts.jpg"},
-    {value: 11, image: "ace_of_diamonds.jpg"},{value: 2, image: "2_of_diamonds.jpg"},
-    {value: 3, image: "3_of_diamonds.jgp"}, {value: 4, image: "4_of_diamonds.jpg"},
-    {value: 5, image: "5_of_diamonds.jpg"}, {value: 6, image: "6_of_diamonds.jpg"},
-    {value: 7, image: "7_of_diamonds.jpg"}, {value: 8, image: "8_of_diamonds.jpg"},
-    {value: 9, image: "9_of_diamonds.jpg"},{value: 10, image: "10_of_diamonds.jpg"},
-    {value: 10, image: "jack_of_diamonds.jpg"},{value: 10, image: "queen_of_diamonds.jpg"},
-    {value: 10, image: "king_of_diamonds.jpg"}];
+    {value: 10, image: "king_of_hearts.jpg"},{value: 11, image: "ace_of_diamonds.jpg"},
+    {value: 2, image: "2_of_diamonds.jpg"}, {value: 3, image: "3_of_diamonds.jpg"},
+    {value: 4, image: "4_of_diamonds.jpg"},{value: 5, image: "5_of_diamonds.jpg"},
+    {value: 6, image: "6_of_diamonds.jpg"}, {value: 7, image: "7_of_diamonds.jpg"},
+    {value: 8, image: "8_of_diamonds.jpg"},{value: 9, image: "9_of_diamonds.jpg"},
+    {value: 10, image: "10_of_diamonds.jpg"},{value: 10, image: "jack_of_diamonds.jpg"},
+    {value: 10, image: "queen_of_diamonds.jpg"},{value: 10, image: "king_of_diamonds.jpg"}];
 
 
 var playingDeck = shuffleDeck(cardDeck);
 var hands = {user:[], dealer:[]};
 var userFinalHandValue;
 var currentPlayer = "user";
-var roundOver = false;
+var roundIsOver = false;
 
 function shuffleDeck(cardDeck){
   return _.shuffle(cardDeck)
@@ -58,10 +54,26 @@ function deal(){
   var dealerFirstCard = getCard();
   hands.dealer.push(dealerFirstCard);
 
+  $('#board').show();
+  $('#deal').hide();
+  setTimeout(function(){
+    var $card = $('<img>').addClass("card").attr({id:"dealerSecond", src:"static/back.jpg"});
+    $("#dealerHand").append($card);}, 200);
+  setTimeout(function(){makeCard(dealerFirstCard, "dealer")}, 600);
+  setTimeout(function(){makeCard(playerCard, "user")}, 900);
+  setTimeout(function(){makeCard(playerCard2, "user")}, 1300);
+  updateHandScore();
+
   if (checkForBlackjack("user")){
-    alert("blackjack!");
-    roundOver = true;
+    setTimeout(function(){alert("blackjack!");
+    roundIsOver = true;}, 200);
+    roundOver();
   }
+}
+
+function makeCard(cardObj, person){
+  var $card = $('<img>').addClass("card").attr('src', 'static/' + cardObj.image);
+  $("#" + person +"Hand").append($card);
 }
 
 function getCard(){
@@ -69,12 +81,15 @@ function getCard(){
   return playingDeck.pop();
 }
 
-
 function getHandValue(person){
   var cards = hands[person];
   return _.sumBy(cards, "value");
 }
 
+function updateHandScore(){
+  setTimeout(function(){$('#userScore')
+    .text("Player's running score: " + getHandValue("user"))}, 1500);
+}
 
 function checkForBlackjack(){
   if (getHandValue("user") === 21){
@@ -83,10 +98,7 @@ function checkForBlackjack(){
   return false;
 }
 
-
 function checkForBust(person){
-  // console.log(hands[person]);
-  // console.log(( _.includes(hands[person], "ace")));
   if (getHandValue(person) < 21){
     return false;
   }
@@ -101,31 +113,38 @@ function checkForBust(person){
   }
 }
 
-
 function hit(){
   var newCard = getCard();
   hands[currentPlayer].push(newCard);
+  makeCard(newCard, currentPlayer);
+  updateHandScore();
 
   if (checkForBust(currentPlayer)){
-      alert("BUST!");
-      roundOver = true;
+      setTimeout(function(){alert("BUST!");
+      roundIsOver = true;}, 300);
+      roundOver();
   }
 }
-
-
 
 function stay(){
   userFinalHandValue = getHandValue("user");
   currentPlayer = "dealer";
   dealerSecondCard();
-  while (roundOver === false){
+  while (roundIsOver === false){
   dealerMove();
-}
+  }
 }
 
 function dealerSecondCard(){
-  hit();
-  //add dealer card to back
+  var secondCard = getCard();
+  hands.dealer.push(secondCard);
+  $("#dealerSecond").attr('src', 'static/' + secondCard.image)
+
+  if (checkForBust(currentPlayer)){
+      setTimeout(function(){alert("BUST!");
+      roundIsOver = true;}, 300);
+      roundOver();
+  }
 }
 
 function dealerMove(){
@@ -138,23 +157,22 @@ function dealerMove(){
 }
 
 function compareHands(dealerSum){
+  roundIsOver = true;
   if (dealerSum === userFinalHandValue){
-    alert("tie!");
+    setTimeout(function(){alert("tie!")}, 200);
   } else if (dealerSum > userFinalHandValue){
-    alert("dealerwins!");
+    setTimeout(function(){alert("dealerwins!")}, 200);
   } else{
-    //user wins
-    alert("user wins!");
+    setTimeout(function(){alert("user wins!")}, 200);
   }
-  roundOver = true;
+  roundOver();
 }
 
+function roundOver(){
+  $("#roundButtons").hide();
+  $("#nextHand").show();
+}
 
 function nextHand(){
-  playingDeck = shuffleDeck(cardDeck);
-  hands = {user:[], dealer:[]};
-  userFinalHandValue;
-  currentPlayer = "user";
-  roundOver = false;
-
+  location.reload();
 }
